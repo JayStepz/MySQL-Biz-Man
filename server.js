945f3fa101fe1bdbd240cf4ce31907f3421ea1b3
JayStepz <jakob_s_stephens@hotmail.com>
2023-04-17 14:20:57 -0400

    Started writing the main function. Main function should be called once db connection is established.

--- server.js
+++ server.js
@@ -9,6 +9,55 @@ const db = mysql.createConnection(
     user: '',
     password: '',
     database: ''
-  },
-  console.log(`Connected to the database.`)
+  }
 );
+
+// Launches main function/app upon database connection
+db.connect(err => {
+  if (err) {
+    console.log('There was an error to connect.')
+  } else {
+    console.log('Connected to the Bizniz Server, boss.')
+  };
+
+  bizApp();
+});
+
+// Main function/app
+function bizApp() {
+  inquirer.prompt({
+    type: 'list',
+    message: 'What would you like to do?',
+    choices: [
+      'View All Departments',
+      'View All Roles',
+      'View All Employees',
+      'Add a Department',
+      'Add a Role',
+      'Add an Employee',
+      'Update Employee Role'
+    ]
+  }).then(function(answer) {
+    if (answer.choice === 'View All Departments') {
+      // View All Departments
+    }
+    if (answer.choice === 'View All Roles') {
+      // View All Roles
+    }
+    if (answer.choice === 'View All Employees') {
+      // View All Employees
+    }
+    if (answer.choice === 'Add a Department') {
+      // Add a Department
+    }
+    if (answer.choice === 'Add a Role') {
+      // Add a Role
+    }
+    if (answer.choice === 'Add an Employee') {
+      // Add an Employee
+    }
+    if (answer.choice === 'Update Employee Role') {
+      // Update Employee Role
+    }
+  });
+}
