3188c80eba478da96ff901bfcddec9fd01997359
JayStepz <jakob_s_stephens@hotmail.com>
2023-04-17 17:51:43 -0400

    progress push before class starts

--- server.js
+++ server.js
@@ -52,15 +52,57 @@ function bizApp() {
     }
     if (answer.choice === 'Add a Department') {
       // Add a Department
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'newDept',
+          message: 'Enter new department name.'
+        }
+      ]).then(function(answer) {
+        db.query('INSERT INTO department', {dept_name: answer.newDept})
+      })
     }
     if (answer.choice === 'Add a Role') {
       // Add a Role
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'newRole',
+          message: 'Enter new employee role.'
+        }
+      ]).then(function(answer) {
+        db.query('INSERT INTO biz_role', {title: answer.newRole})
+      })
     }
     if (answer.choice === 'Add an Employee') {
       // Add an Employee
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'newFirstName',
+          message: 'Enter new employee first name'
+        },
+        {
+          type: 'input',
+          name: 'newLastName',
+          message: 'Enter new employee last name'
+        }
+      ]).then(function(answer) {
+        db.query('INSERT INTO employee', {first_name: answer.newFirstName}, {last_name: answer.newLastName})
+      })
     }
     if (answer.choice === 'Update Employee Role') {
       // Update Employee Role
+      inquirer.prompt([
+        {
+          type: 'list',
+          name: 'upRole',
+          message: 'Which employee is changing roles?',
+          choices: [
+            
+          ]
+        }
+      ])
     }
   });
 }
